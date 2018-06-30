4679e53b7cf8de36d5bdabd51dfce24e3fbbd878
LuisStivenCruz <stivens-96@hotmail.com>
2018-06-29 19:54:34 -0500

    ModificarYVisualizar
    Modifica un vector

---
+++ ModificarYVisualizacion.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#define MAX 100
+using namespace std;
+int main (void)
+{
+	int n, pos, num, v[MAX];
+	cout<<"\n Ingrese numero de elementos: ";
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		cout<<"\n Ingrese en la posicion: ["<<i<<"]: ";
+		cin>>v[i];
+	}
+	cout<<"\n Ingrese la posicion de elemento a modificar: ";
+	cin>>pos;
+	cout<<"\n Ingrese el numero a remplazar: ";
+	cin>>num;
+	for(int i=n;i>=pos;i--)
+	{
+		i=pos;
+		v[i]=num;
+	}
+	for(int i=1;i<=n;i++)
+	{
+		cout<<"\n "<<v[i]<<"\n";
+	}
+}
+/*Diseñar un programa que permita ingresar
+valores en un vector de n elementos,
+luego el sw deberá permitir 
+la modificación de uno de los
+elementos del arreglo, 
+desde una posición especifica.
+*/
